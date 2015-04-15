15d1b151fbff85e530471f864b17de9ad074c3b4
YonatanSimson <simsonyee@gmail.com>
2015-04-15 22:18:51 +0300

    Changing Dy to sparse. TBD make Dx sparse

--- HW1/CreateDerivativeOperators.m
+++ HW1/CreateDerivativeOperators.m
@@ -5,23 +5,56 @@
 
 function [Dx, Dy] = CreateDerivativeOperators(X_rows, X_cols)
 
-%% formulate the L matrix
-%Dy - diff(X, 1, 1)
-idx = 1;
+%% formulate the Dx, Dy matrices
+%Dy ~ diff(X, 1, 1)
+idx = 1;%row index on output matrix
+ii = zeros(X_cols*X_rows*2, 1);
+jj = zeros(X_cols*X_rows*2, 1);
+v  = zeros(X_cols*X_rows*2, 1);
+
 Dy = zeros(X_cols*X_rows);
 for n = 1:X_cols,
     Dy(idx, idx)     = -1;
     Dy(idx, idx + 1) = 1;
+    ii(2*idx-1) = idx;
+    jj(2*idx-1) = idx;
+    v(2*idx-1)  = -1;
+    
+    ii(2*idx) = idx;
+    jj(2*idx) = idx + 1;
+    v(2*idx)  = 1;
+    
+
     idx = idx + 1;
     for m = 2:X_rows-1,
         Dy(idx, idx - 1) = -0.5;
         Dy(idx, idx + 1) = 0.5;
+        
+        ii(2*idx-1) = idx;
+        jj(2*idx-1) = idx - 1;
+        v(2*idx-1)  = -0.5;
+
+        ii(2*idx) = idx;
+        jj(2*idx) = idx + 1;
+        v(2*idx)  = 0.5;
+        
+        
         idx = idx + 1;
     end
     Dy(idx, idx - 1) = -1;
     Dy(idx, idx)     = 1;
+    
+    ii(2*idx-1) = idx;
+    jj(2*idx-1) = idx - 1;
+    v(2*idx-1)  = -1;
+    
+    ii(2*idx) = idx;
+    jj(2*idx) = idx;
+    v(2*idx)  = 1;
+    
     idx = idx + 1;
 end
+Dy = sparse(ii, jj, v);
 
 %Dx - diff(X, 1, 2)
 idx = 1;
@@ -45,3 +78,5 @@ for m = 1:X_rows,
     Dx(idx, idx - X_rows) = -1;
     idx = idx + 1;
 end
+
+Dx = sparse(Dx);
