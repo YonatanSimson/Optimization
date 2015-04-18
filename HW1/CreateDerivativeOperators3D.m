42269df6b3877919a3935f5a96a6ad1530ad1e56
YonatanSimson <simsonyee@gmail.com>
2015-04-18 09:10:25 +0300

    Finished Dx, Dy. Dz still has a bug

--- HW1/CreateDerivativeOperators3D.m
+++ HW1/CreateDerivativeOperators3D.m
@@ -13,60 +13,61 @@ ii = zeros(X_cols*X_rows*X_dim*2, 1);
 jj = zeros(X_cols*X_rows*X_dim*2, 1);
 v  = zeros(X_cols*X_rows*X_dim*2, 1);
 
+Dx = sparse(X_cols*X_rows*X_dim, X_cols*X_rows*X_dim);
 Dy = sparse(X_cols*X_rows*X_dim, X_cols*X_rows*X_dim);
-% Dy = [];
+[Dx2d, Dy2d] = CreateDerivativeOperators(X_rows, X_cols);
+
 size2d = X_cols*X_rows;
+
 for l = 1:X_dim,
-    [Dx2D, Dy2d] = CreateDerivativeOperators(X_rows, X_cols);
-    Dy((l-1)*size2d + (1:size2d), (l-1)*size2d + (1:size2d)) = Dy2d;
+    Dx((l-1)*size2d + (1:size2d), (l-1)*size2d + (1:size2d)) = Dx2d; 
+    Dy((l-1)*size2d + (1:size2d), (l-1)*size2d + (1:size2d)) = Dy2d; %#ok<SPRIX>
+    
 end
-Dy = sparse(ii, jj, v);
 
-%Dx ~ diff(X, 1, 2)
+%Dz ~ diff(X, 1, 3)
 idx = 1;
-ii = zeros(X_cols*X_rows*2, 1);
-jj = zeros(X_cols*X_rows*2, 1);
-v  = zeros(X_cols*X_rows*2, 1);
+ii = zeros(size2d*X_dim*2, 1);
+jj = zeros(size2d*X_dim*2, 1);
+v  = zeros(size2d*X_dim*2, 1);
 
-for m = 1:X_rows,
+for m = 1:size2d,
     
     ii(2*idx-1) = idx;
     jj(2*idx-1) = idx;
     v(2*idx-1)  = -1;
     
     ii(2*idx) = idx;
-    jj(2*idx) = idx + X_rows;
+    jj(2*idx) = idx + size2d;
     v(2*idx)  = 1;
     idx = idx + 1;
 end
 
-for n = 2:X_cols-1,
-    for m = 1:X_rows,
+for n = 2:size2d-1,
+    for m = 1:X_dim,
         ii(2*idx-1) = idx;
-        jj(2*idx-1) = idx - X_rows;
+        jj(2*idx-1) = idx - size2d;
         v(2*idx-1)  = -0.5;
 
         ii(2*idx) = idx;
-        jj(2*idx) = idx + X_rows;
+        jj(2*idx) = idx + size2d;
         v(2*idx)  = 0.5;
         idx = idx + 1;
     end
 end
 
-for m = 1:X_rows,
+for m = 1:size2d,
 
     ii(2*idx-1) = idx;
     jj(2*idx-1) = idx;
     v(2*idx-1)  = 1;
     
     ii(2*idx) = idx;
-    jj(2*idx) = idx - X_rows;
+    jj(2*idx) = idx - size2d;
     v(2*idx)  = -1;
 
     idx = idx + 1;
 end
 
-Dx = sparse(ii, jj, v);
+Dz = sparse(ii, jj, v);
 
-%Dz~ diff(X, 1, 3)
-Dz = zeros(size(Dx));
