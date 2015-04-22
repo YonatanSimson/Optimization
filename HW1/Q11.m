7c21414f8470ec470ac18b4765821f5bd6d20ac1
YonatanSimson <simsonyee@gmail.com>
2015-04-22 18:38:12 +0300

    Made improvements to lsqr solution- managed to reduce residual

--- HW1/Q11.m
+++ HW1/Q11.m
@@ -1,7 +1,7 @@
 %% Q11
 clear; close all;
-load('small\A');
-load('small\y');
+load('Small\A');
+load('Small\y');
 
 rows = 19;
 cols = 19;
@@ -14,10 +14,36 @@ L = [Dx; Dy; Dz];
 AA = [A; sqrt(lambda)*L];
 B = [y; zeros(size(L, 1), 1)];
 %Solution using lsqr
-x_ls = lsqr(AA, B, 1e-6, 200);
+x_ls = lsqr(AA, B, 1e-10, 1000);
 x_ls = reshape(x_ls, [rows, cols, dim]);
 %solution using inverse - takes a while
 Xest = ((A'*A) + lambda*(L'*L))\(A'*y);
+
+residual_ls = sum(sum((x_ls(:) - Xest(:)).^2));
+
+X = reshape(x_ls, [rows, cols, dim]);
+displayVolumeSliceGUI(X);
+
+%% Large
+clear; close all;
+load('Large\A');
+load('Large\y');
+
+rows = 49;
+cols = 49;
+dim  = 49;
+[Dx, Dy, Dz] = CreateDerivativeOperators3D(rows, cols, dim);
+
+lambda = 1e-5;
+
+L = [Dx; Dy; Dz];
+AA = [A; sqrt(lambda)*L];
+B = [y; zeros(size(L, 1), 1)];
+%Solution using lsqr
+x_ls = lsqr(AA, B, 1e-6, 400);
+x_ls = reshape(x_ls, [rows, cols, dim]);
+%solution using inverse - takes a while
+Xest = ((A'*A) + lambda*(L'*L))\(A'*y);%does not work for the large solution
 
 residual_ls = sum(sum((x_ls(:) - Xest(:)).^2));
 
