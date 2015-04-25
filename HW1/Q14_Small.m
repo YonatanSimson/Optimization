b8a6efe9b8f307b0bcb975cacbab526745a3e22f
YonatanSimson <anon@gmail.com>
2015-04-25 13:13:48 +0300

    Adding solution for small problem

---
+++ HW1/Q14_Small.m
@@ -0,0 +1,34 @@
+%Toy IRLS
+clear; close all;
+load('Small\A');
+load('Small\y');
+
+rows = 19;
+cols = 19;
+dim  = 19;
+[Dx, Dy, Dz] = CreateDerivativeOperators3D(rows, cols, dim);
+
+lambda = 1e-3;
+
+
+L = [Dx; Dy; Dz];
+AA = [A; sqrt(lambda)*L];
+B = [y; zeros(size(L, 1), 1)];
+
+%Solution using lsqr
+x_ls = lsqr(AA, B, 1e-12, 5000);
+x_ls = reshape(x_ls, [rows, cols, dim]);
+
+%solution using inverse - takes a while
+Xest = ((A'*A) + lambda*(L'*L))\(A'*y);
+residual_ls = sum(sum((x_ls(:) - Xest(:)).^2));
+
+%% IRLS on previous solution
+lambda = 1;
+epsilon = 0.0001;%for reweighting matrix
+maxIter = 5000;
+tol     = 1e-10;
+[x, cost] = IRLS(A, L, y, x_ls(:), epsilon, lambda, maxIter, tol);
+figure(1); plot(1:length(cost), cost); xlabel('iter'); title('Cost')
+X = reshape(x, [rows, cols, dim]);
+displayVolumeSliceGUI(X);
