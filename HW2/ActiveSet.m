83d8156ad0fc26deefb98efbad6961f1092c5be8
YonatanSimson <anon@gmail.com>
2015-05-24 20:54:07 +0300

    Fixed bugs in projected Newton

--- HW2/ActiveSet.m
+++ HW2/ActiveSet.m
@@ -1,3 +1,4 @@
-function IPlus = ActiveSet(gradf_x, epsilon)
+function IPlus = ActiveSet(x, gradf_x, lb, ub, epsilon)
 
-IPlus = abs(gradf_x) > epsilon;
+IPlus = (gradf_x > 0 & lb <= x           & x <= lb + epsilon) | ...
+        (gradf_x < 0 & ub - epsilon <= x & x <= ub);
--- HW2/ProjectedNewtonToy.m
+++ HW2/ProjectedNewtonToy.m
@@ -2,8 +2,8 @@
 
 H=[5 -2 -1; -2 4 3; -1 3 5];
 c=[2; -35; -47];
-lb = [0; 0; 0];
-ub = [6; 6; 6];
+lb = [6; 6; 6];
+ub = [8; 8; 8];
 options = optimoptions('quadprog',...
     'Algorithm','interior-point-convex','Display','final-detailed');
 x_ref = quadprog(H,c,[],[],[], [], lb,ub, [], options);
@@ -17,13 +17,14 @@ f = @(x)(0.5*x'*H*x+c'*x);
 gradf = @(x)(H*x+c);
 
 %params
-maxIter = 1000;
-tol = 1e-8;
-maxIterInner = 1000;
-tolInner = 1e-10;
-sigma = 0.2;
-beta  = 0.6;
-flag = 1;
+maxIter = 1000;%For Projected Newton
+tol = 1e-8;%For Projected Newton
+maxIterInner = 1000;% For CG - for inverting Hessian. Inner loop
+tolInner = 1e-10;% For CG - for inverting Hessian. Inner loop
+sigma = 0.2;% For Armijo rule
+beta  = 0.6;% For Armijo rule
+flag = 1;%0 - unconstrained, 1-constrained case with box constraints
+epsilon = 1e-6;%for active set
 %Init
 x0 = 0.5*(lb + ub);
 alpha_k = 0.9;
@@ -40,10 +41,7 @@ for k = 1:maxIter,
     end
     gradf_x = gradf(x);
     % Find active set
-    IPlus = ActiveSet(gradf_x, 1e-6);
-    if (sum(~IPlus))
-        disp('debug')
-    end
+    IPlus = ActiveSet(x, gradf_x, lb, ub, 1e-6);
     active    = find(IPlus);
     nonActive = find(~IPlus);
     P  = sparse(1:dim, [nonActive; active;], ones(dim, 1));%permuation matrix
