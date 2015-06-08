e25565ea643024271b4250f0cc51c4c73a8f9afd
YonatanSimson <anon@gmail.com>
2015-06-08 21:09:45 +0300

    Yonatan: Code clean up

--- HW2/GradientProjectionDemo.m
+++ HW2/GradientProjectionDemo.m
@@ -8,9 +8,6 @@ options = optimoptions('quadprog',...
     'Algorithm','interior-point-convex','Display','final-detailed');
 x_ref = quadprog(H,c,[],[],[], [], lb,ub, [], options);
 x_unc = quadprog(H,c,[],[],[], [], [],[], [], options);
-maxIterInner = 1000;
-tolInner = 1e-10;
-tolkkt = 1e-3;
 tol = 1e-8;
 maxIter = 100;
 x_cg = ConjGrad(H,-c,[3; 3; 3], maxIter, tol);
