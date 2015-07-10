f7af3f9723a3b3f964e9d5e6f86007bb06695954
YonatanSimson <anon@gmail.com>
2015-07-11 00:10:09 +0300

    Fixing bug in Q17

--- HW3/Q17.m
+++ HW3/Q17.m
@@ -5,11 +5,17 @@ clear all
 load('I.mat');
 load('mozart.mat');
 
+figure;
+imagesc(I);
+axis image
+colormap gray
+title('Shaded image')
+
 eps = 1E-10;
 F       = sqrt(1./I.^2 - 1 );
 Fe    = F + eps * (F == 0);
 
-x0 = [128,145];
+x0 = [121, 143];
 z0 = 0;
 z  = runFSM(Fe,x0,z0);
 
@@ -24,13 +30,6 @@ axis('tight');
 view(110,45);
 axis('off');
 camlight
+title('|\nablaz|=F(x,y)')
 
-figure; 
-surf(mozart); 
-colormap gray
-shading interp;
-axis('tight');
-view(110,45);
-axis('off');
-camlight
 
