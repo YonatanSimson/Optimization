34b3bf1d24b6d146de65e80be7e807b6cab3b88b
YonatanSimson <anon@gmail.com>
2015-07-11 21:55:13 +0300

    Minor bug corrections

--- HW3/Q17.m
+++ HW3/Q17.m
@@ -3,7 +3,6 @@ close all
 clear all
 
 load('I.mat');
-load('mozart.mat');
 
 figure;
 imagesc(I);
@@ -12,15 +11,15 @@ colormap gray
 title('Shaded image')
 
 eps = 1E-10;
-F       = sqrt(1./I.^2 - 1 );
-Fe    = F + eps * (F == 0);
+F   = sqrt(1./I.^2 - 1 );
+Fe  = F + eps * (F == 0);
 
 x0 = [121, 143];
 z0 = 0;
 z  = runFSM(Fe,x0,z0);
 
-z11    = z(1,1);
-z      = -z + z11;
+z = -z;
+z = (z - min(z(:)))/(max(z(:)) - min(z(:)));
 
 figure; 
 surf(z); 
