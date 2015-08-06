659c4bfc1f728b73bcf0cf5be197269fee2ffa8e
YonatanSimson <anon@gmail.com>
2015-08-06 23:50:40 +0300

    Adding functions Using transform to get from h->h

--- HW4/Q5.m
+++ HW4/Q5.m
@@ -20,6 +20,8 @@ for N = 5:2:101,
     displayResults(h, 2)
     disp(['N: ' num2str(N)])
     disp(['delta: ' num2str(delta)])
+    %Once delta is smaller than 1 we know that we satisfy the design
+    %constraints
     if ( delta < 1 )
         break;
     end
@@ -28,7 +30,7 @@ end
 %% Nested Functions
     function [del, h] = EquirippleDesign(S, H_d)
         M = (N-1)/2;
-        C = [ones(L+1, 1) 2*cos(w'*[1:M])];
+        C = [ones(L+1, 1) cos(w'*[1:M])];
         
         A = [ S*C -ones(L+1,1);
              -S*C -ones(L+1,1);];
@@ -41,24 +43,10 @@ end
 
         del = x(end);
         a = x(1:M+1);
-        h = [a(M+1:-1:2); a(1); a(2:M+1)];
-    end
-
-    function displayResults(hh, fsamp)
-        figure(1);
-        subplot(2,1,1)
-        [H,f] = freqz(hh,1,1024,fsamp);
-        semilogy(f,abs(H)), grid on
-        title('Frequency responce F\{h_d[n]\}')
-        ylabel('|H_d(\omega)|')
-        xlabel('Frequency[Hz]')
-
-        subplot(2,1,2)
-        stem(0:length(hh)-1, hh);
-        title('Impulse responce')
-        ylabel('h_d[n]');
-        xlabel('n');
-
+%         h = [0.5*a(M+1:-1:2); a(1); 0.5*a(2:M+1)];
+        T2 = aToh(N);
+        h = T2*a;
+        
     end
 
 end
---
+++ HW4/aToh.m
@@ -0,0 +1,7 @@
+function T2 = aToh(N)
+    M = (N-1)/2;
+
+    T2 = 1/2*eye(M+1);
+    T2 = [flipud(T2(2:end, :)); T2];
+    T2(M + 1, 1) = 1;
+end
